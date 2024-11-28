2e7b3b7868301c71f99f85737e42fef65214e3f6
Jesus Baak <jesusbaak@hotmail.com>
2024-11-28 13:25:59 -0600

    Se termina crud de mascotas

--- controllers/mascotas.js
+++ controllers/mascotas.js
@@ -7,7 +7,7 @@ class mascotasController {
 
     async create(req, res) {
         try {
-            const data = mascotasModel.create(req.body);
+            const data = await mascotasModel.create(req.body);
             res.status(201).json(data)
         } catch (e) {
             res.status(500).send(e);
@@ -16,7 +16,9 @@ class mascotasController {
 
     async update(req, res) {
         try {
-            res.status(201).json({ status: 'ok'})
+            const { id }  = req.params;
+            const data = await mascotasModel.update(id, req.body);
+            res.status(200).json(data)
         } catch (e) {
             res.status(500).send(e);
         }
@@ -24,7 +26,9 @@ class mascotasController {
 
     async delete(req, res) {
         try {
-            res.status(201).json({ status: 'ok'})
+            const { id }  = req.params;
+            const data = await mascotasModel.delete(id);
+            res.status(206).json(data)
         } catch (e) {
             res.status(500).send(e);
         }
@@ -32,15 +36,19 @@ class mascotasController {
 
     async getAll(req, res) {
         try {
-            res.status(201).json({ status: 'ok-get'})
+            const data = await mascotasModel.getAll();
+            res.status(200).json(data)
         } catch (e) {
             res.status(500).send(e);
         }
     }
 
     async getOne(req, res) {
+        
         try {
-            res.status(201).json({ status: 'ok'})
+            const { id }  = req.params;
+            const data = await mascotasModel.getOne(id);
+            res.status(200).json(data)
         } catch (e) {
             res.status(500).send(e);
         }
